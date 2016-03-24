3bc4d05b228abcb029f72d6b1bc0b2267f7a2d90
itsFrank <frankobrien11@yahoo.ca>
2016-03-24 01:43:08 -0400

    fixed write

--- REGISTER_FILE.vhd
+++ REGISTER_FILE.vhd
@@ -20,18 +20,21 @@ architecture arch of REGISTER_FILE is
 	signal register_file : register_file_t := ((others=> (others=>'0')));
 begin
 
-	register_pro : process( clk )
+	register_pro : process( reg_1_select, reg_2_select )
 	begin
-		if rising_edge(clk) then
 			reg_1_data <= register_file(to_integer(unsigned(reg_1_select)));
 			reg_2_data <= register_file(to_integer(unsigned(reg_2_select)));
-			
+	end process ; -- register_pro
+
+	write_back_pro : process( clk )
+	begin
+		if falling_edge(clk) then
 			if write_en = '1' then
 				if write_select /= "00000" then
 					register_file(to_integer(unsigned(write_select))) <= write_data;
 				end if;
 			end if ;
 		end if;
-	end process ; -- register_pro
+	end process ; -- write_back_pro
 
 end architecture ; -- arch
